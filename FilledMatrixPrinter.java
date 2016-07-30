3dc4d3c398f1357fd3ff3f7170feb87b53c7c8e6
a-telegin <telegin.andrii@gmail.com>
2016-07-30 16:57:24 +0300

    Update FilledMatrixPrinter.java

--- FilledMatrixPrinter.java
+++ FilledMatrixPrinter.java
@@ -8,10 +8,8 @@ class FilledMatrixPrinter{
 	
 	static void MtxPrint(int rows, int cols, int filler){
 		if (rows < 0 || cols < 0) throw new IllegalArgumentException("Illegal input!");
-		
 		else{
 			int arr[][] = new int[rows][cols];
-			
 			for (int i = 0; i < rows; i++){
 				for (int k = 0; k < cols; k++){
 					arr[i][k] = filler;
